b64915fbd219e92f73454a248ecb22c8b091e4b6
DylanSS98 <72262587+DylanSS98@users.noreply.github.com>
2020-10-20 15:51:41 +0200

    add modification.php and style bootstrap

--- form_modification.php
+++ form_modification.php
@@ -4,7 +4,7 @@ $pdo = new PDO('mysql:host=mysql;dbname=basedetest;host=127.0.0.1', 'root', "", 
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 
-$query = $pdo->prepare('SELECT FROM connexion WHERE id=:motdepasse');
+$query = $pdo->prepare('SELECT * FROM connexion WHERE id=:motdepasse');
 
 $query->bindValue(':motdepasse', $_GET['mdpUtilisateur'], PDO::PARAM_INT);
 
@@ -17,21 +17,23 @@ $utilisateur = $query->fetch();
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="style_index.css">
     <meta charset="utf-8" />
     <title>Formulaire de modification</title>
 </head>
 
 <body>
 
-    <form action="cour.php" method="POST">
+    <form action="modification.php" method="POST">
+        <input type="hidden" name="mdpUtilisateur" value="<?=  $utilisateur['id']?>">
         <div>
             <label for="">Email :</label>
-            <input type="email" name="mail" value="<?= $utilisateur['email'] ?>">
+            <input type="email" name="mail" value="<?= $utilisateur['email']; ?>">
         </div>
 
         <div>
             <label for="">Mot de passe :</label>
-            <input type="password" name="motdepasse" value="<?= $utilisateur['motdepasse'] ?>">
+            <input type="password" name="motdepasse" value="<?= $utilisateur['motdepasse']; ?>">
         </div>
 
         <button type="submit">Enregistrer</button>
--- modification.php
+++ modification.php
@@ -4,23 +4,30 @@ $pdo = new PDO('mysql:host=mysql;dbname=basedetest;host=127.0.0.1', 'root', "", 
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 
-$query = $pdo->prepare('DELETE FROM connexion WHERE id=:motdepasse LIMIT 1');
+$query = $pdo->prepare ('UPDATE connexion set email=:email, motdepasse=:motdepasse WHERE id=:num LIMIT 1');
 
-$query->bindValue(':motdepasse', $_GET['mdpUtilisateur'], PDO::PARAM_INT);
+$query->bindValue(':num', $_POST['mdpUtilisateur']);
+
+$query->bindValue(':email', $_POST['mail']);
+
+$query->bindValue(':motdepasse', $_POST['motdepasse']);
 
 $executeIsOk = $query->execute();
 
 if ($executeIsOk){
     echo '<div class="alert alert-success" role="alert">
             <link rel="stylesheet" href="style_index.css">
-    Utilisateur supprimé !
+    Le contact à été mis a jour !
     </div>';
 }
-else {
-    echo '<div class="alert alert-danger" role="alert">
+else{
+     echo '<div class="alert alert-danger" role="alert">
 <link rel="stylesheet" href="style_index.css">
-    Echec de la suppression !
-    </div>';
+     Echec de la mise a jour
+     </div>';
 }
 header ("Refresh: 3;URL=http://localhost/test_php/bdd.php");
 exit();
+?>
+
+
